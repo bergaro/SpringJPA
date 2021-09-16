2fe83b9e35d5dfca132d5c6d337a5a23dc174d1b
Andrey <kriteriykoshi@gmail.com>
2021-09-16 14:46:53 +0300

    fix PersonRepository

--- src/main/java/ru/netology/hibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernate/repository/PersonRepository.java
@@ -3,11 +3,13 @@ package ru.netology.hibernate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.netology.hibernate.repository.PersonEntity.Person;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     @Query("select p from persons p where p.cityOfLiving = :city")
